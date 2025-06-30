005266210338e64f3ea0946076929d2f97b3370e
Hans Jacob Melby <hansj.melby@gmail.com>
2025-06-30 14:20:36 +0200

    legger til webflux og co-rutiner

--- src/main/kotlin/no/nav/persondataapi/Application.kt
+++ src/main/kotlin/no/nav/persondataapi/Application.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @SpringBootApplication
 class Application
 
