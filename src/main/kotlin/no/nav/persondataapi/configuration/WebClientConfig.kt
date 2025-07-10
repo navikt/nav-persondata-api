659e88775f6d58195d83bc17357fb6a4e173c576
Hans Jacob Melby <hansj.melby@gmail.com>
2025-07-10 20:12:06 +0200

    endring i WebClientConfig.kt

--- src/main/kotlin/no/nav/persondataapi/configuration/WebClientConfig.kt
+++ src/main/kotlin/no/nav/persondataapi/configuration/WebClientConfig.kt
@@ -3,6 +3,7 @@ package no.nav.persondataapi.configuration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.context.request.RequestContextListener
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -11,6 +12,10 @@ class WebClientConfig {
 
     @Value("\${NAIS_TOKEN_EXCHANGE_ENDPOINT}")
     lateinit var tokenExchangeUrl: String
+    @Value("\${UTBETALING_URL}")
+    lateinit var utbetalingURL: String
+
+
 
     @Bean
     fun tokenWebClient(builder: WebClient.Builder): WebClient =
@@ -21,7 +26,11 @@ class WebClientConfig {
     @Bean
     fun utbetalingWebClient(builder: WebClient.Builder): WebClient =
         builder
-            .baseUrl("https://sokos-utbetaldata.dev.intern.nav.no/utbetaldata/api/v2/hent-utbetalingsinformasjon/intern")
-            .defaultHeader("Content-Type", "application/json")
+            .baseUrl("$utbetalingURL/utbetaldata/api/v2/hent-utbetalingsinformasjon/intern")
+            //.defaultHeader("Content-Type", "application/json")
+            .defaultHeaders {
+                it.accept = listOf(MediaType.APPLICATION_JSON)
+                it.contentType = MediaType.APPLICATION_JSON
+            }
             .build()
 }
