375226a789564a37be62c9a9cb72015c2f35a5fe
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2026-01-22 14:10:46 +0100

    Legg til logging av at saksbehandler bruker løsningen (#198)

--- src/main/kotlin/no/nav/persondataapi/service/SaksbehandlerService.kt
+++ src/main/kotlin/no/nav/persondataapi/service/SaksbehandlerService.kt
@@ -3,6 +3,7 @@ package no.nav.persondataapi.service
 import no.nav.persondataapi.integrasjon.nom.NomClient
 import no.nav.persondataapi.integrasjon.nom.SaksbehandlerTilhørighetResultat
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +11,9 @@ class SaksbehandlerService(
     private val tokenValidationContextHolder: TokenValidationContextHolder,
     private val nomClient: NomClient,
 ) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     /**
      * Henter organisasjonstilhørighet for innlogget saksbehandler.
      */
@@ -18,6 +22,13 @@ class SaksbehandlerService(
         val token = context.firstValidToken ?: throw IllegalStateException("Fant ikke gyldig token")
         val navIdent = token.jwtTokenClaims.get("NAVident").toString()
 
-        return nomClient.hentSaksbehandlerTilhørighet(navIdent)
+        val resultat = nomClient.hentSaksbehandlerTilhørighet(navIdent)
+
+        if (resultat.errorMessage == null && resultat.data != null) {
+            logger.info("Saksbehandler $navIdent i ${resultat.data.organisasjoner.joinToString(", ")} lastet applikasjonen")
+        } else {
+            logger.warn("Feil ved henting av organisasjonstilhørighet for ${navIdent}: ${resultat.errorMessage}")
+        }
+        return resultat
     }
 }
