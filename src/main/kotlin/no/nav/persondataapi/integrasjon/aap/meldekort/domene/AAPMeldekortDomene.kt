d099e8dd4ce537afcfbe7c11bbb7ef2e9c2ec8d8
Hans Jacob Melby <hansj.melby@gmail.com>
2026-01-22 11:14:13 +0100

    modell for mottak av data fra aap max endepunkt

---
+++ src/main/kotlin/no/nav/persondataapi/integrasjon/aap/meldekort/domene/AAPMeldekortDomene.kt
@@ -0,0 +1,51 @@
+package no.nav.persondataapi.integrasjon.aap.meldekort.domene
+
+import java.time.LocalDate
+
+data class AAPMeldekortRespons(
+    val vedtak: List<Vedtak>
+)
+data class Vedtak(
+    val vedtakId: String,
+    val status: String,
+    val saksnummer: String,
+    val vedtaksdato: String, // evt. LocalDate
+    val periode: Periode,
+    val rettighetsType: String,
+
+    val dagsats: Int,
+    val dagsatsEtterUforeReduksjon: Int?,
+    val beregningsgrunnlag: Int,
+
+    val barnMedStonad: Int,
+    val barnetillegg: Int,
+
+    val kildesystem: String,
+
+    val samordningsId: String?,
+    val opphorsAarsak: String?,
+    val vedtaksTypeKode: String?,
+    val vedtaksTypeNavn: String?,
+
+    val utbetaling: List<Utbetaling>
+)
+data class Utbetaling(
+    val periode: Periode,
+    val belop: Int,
+    val dagsats: Int,
+    val utbetalingsgrad: Int,
+
+    val reduksjon: Reduksjon?,
+    val barnetilegg: Int?,
+    val barnetillegg: Int?
+)
+data class Periode(
+    val fraOgMedDato: LocalDate, // evt. LocalDate
+    val tilOgMedDato: LocalDate  // evt. LocalDate
+)
+
+data class Reduksjon(
+    val annenReduksjon : String,
+    val timerArbeidet : String
+
+)
---
+++ src/test/kotlin/no/nav/persondataapi/parsing/AapMeldekortResponsTest.kt
@@ -0,0 +1,30 @@
+package no.nav.persondataapi.parsing
+
+import no.nav.persondataapi.integrasjon.aap.meldekort.domene.AAPMeldekortRespons
+import no.nav.persondataapi.konfigurasjon.JsonUtils
+import org.springframework.core.io.ClassPathResource
+import org.springframework.util.StreamUtils
+import java.nio.charset.StandardCharsets
+import kotlin.test.Test
+
+class AapMeldekortResponsTest {
+
+    @Test
+    fun KanLese2025Respons(){
+        val jsonString = lesJsonFraFil("testrespons/AAPMaxRepons2025.json")
+        val aapMeldekortRespons: AAPMeldekortRespons = JsonUtils.fromJson(jsonString)
+        println()
+    }
+    @Test
+    fun KanLese2026Respons(){
+        val jsonString = lesJsonFraFil("testrespons/AAPMaxRespons2026.json")
+        val aapMeldekortRespons: AAPMeldekortRespons = JsonUtils.fromJson(jsonString)
+        println()
+    }
+
+}
+private fun lesJsonFraFil(filename: String): String {
+    val resource = ClassPathResource(filename)
+    val inputStream = resource.inputStream
+    return StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8)
+}
